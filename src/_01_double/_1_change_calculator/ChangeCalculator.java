043f61244dbdc02a234acfbe386b3c3b798c3958
JamesQian123 <jamesqian12345678910@gmail.com>
2022-04-23 12:46:36 -0700

    a

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		int	nickels = Integer.parseInt(JOptionPane.showInputDialog(null,"How many nickels do you have?"));
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
 
 		// Ask the user how many dimes they have, and convert their answer
-
+		int dimes = Integer.parseInt(JOptionPane.showInputDialog(null,"How many dimes do you have?"));
+				
 		// Ask the user how many quarters they have, and convert their answer
-
+		int quarters = Integer.parseInt(JOptionPane.showInputDialog(null,"How many quarters do you have"));
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+		double total = nickels * 0.05 + dimes * 0.1 + quarters * 0.25 ;
+			
+		
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+		JOptionPane.showMessageDialog(null,"you have $"+total);
 	}
 }
 
